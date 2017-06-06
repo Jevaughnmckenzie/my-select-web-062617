69bc9ec321a9be5002fce6ab84e915458866df9d
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-06-06 01:40:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
  # your code here!
+ if collection.empty?
+   return nil
+ end
+
+ i = 0
+ new_array = []
+
+ while i < collection.count
+   test = yield collection[i]
+   if (test)
+    new_array << collection[i]
+   end
+    i += 1
+ end
+ new_array
 end
